94ecee3a2f18a34b4357e628b6ef5babd0f70ad9
hashar7 <86832867+hashar7@users.noreply.github.com>
2023-04-04 17:37:29 +0300

    Changed mean to median

--- solution.py
+++ solution.py
@@ -5,4 +5,4 @@ import numpy as np
 chat_id = 458704720 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    return 2 * np.mean(x) / (42 ** 2)
+    return 2 * np.median(x) / (42 ** 2)
